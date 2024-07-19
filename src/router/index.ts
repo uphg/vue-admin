48c9f709451f8fb20ba6d57c11a2fb607443a191
lvheng <1834805770@qq.com>
2024-07-19 14:15:46 +0800

    feat: use antfu eslint config.

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,11 +20,11 @@ const router = createRouter({
         }
       ]
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/About.vue')
-    }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('../views/About.vue')
+    // }
   ]
 })
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app-container home">
     <h2>首页</h2>
+    <div class="c-red bg-white" flex="~ col" bg="red" color="red"></div>
   </div>
 </template>
